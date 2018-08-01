654bd6c5e7b2d15ce927adf6d028a49fcf7ed227
Yohei Munesada <y.munesada@gmail.com>
2018-08-01 17:56:55 +0900

    seedデータを作成

---
+++ sql/database/seeds.sql
@@ -0,0 +1,21 @@
+# ユーザ.
+INSERT users VALUES (1, "test1", "test1", "うさぎ", "token1", "/static/images/icons/1.png");
+INSERT users VALUES (2, "test2", "test2", "くま", "token2", "/static/images/icons/2.png");
+INSERT users VALUES (3, "test3", "test3", "ねずみ", "token3", "/static/images/icons/3.png");
+INSERT users VALUES (4, "test4", "test4", "いぬ", "token4", "/static/images/icons/4.png");
+INSERT users VALUES (5, "test5", "test5", "ぱんだ", "token5", "/static/images/icons/5.png");
+
+# 投稿.
+INSERT posts VALUES (1, 1, "/static/images/photos/1.jpg", "本文だよー本文だよー1", now());
+INSERT posts VALUES (2, 1, "/static/images/photos/2.jpg", "本文だよー本文だよー2", now());
+INSERT posts VALUES (3, 1, "/static/images/photos/3.jpg", "本文だよー本文だよー3", now());
+INSERT posts VALUES (4, 2, "/static/images/photos/4.jpg", "本文だよー本文だよー4", now());
+INSERT posts VALUES (5, 2, "/static/images/photos/5.jpg", "本文だよー本文だよー5", now());
+INSERT posts VALUES (6, 2, "/static/images/photos/6.jpg", "本文だよー本文だよー6", now());
+INSERT posts VALUES (7, 3, "/static/images/photos/7.jpg", "本文だよー本文だよー7", now());
+INSERT posts VALUES (8, 3, "/static/images/photos/8.jpg", "本文だよー本文だよー8", now());
+INSERT posts VALUES (9, 3, "/static/images/photos/9.jpg", "本文だよー本文だよー9", now());
+INSERT posts VALUES (10, 4, "/static/images/photos/10.jpg", "本文だよー本文だよー10", now());
+INSERT posts VALUES (11, 4, "/static/images/photos/11.jpg", "本文だよー本文だよー11", now());
+INSERT posts VALUES (12, 5, "/static/images/photos/12.jpg", "本文だよー本文だよー12", now());
+INSERT posts VALUES (13, 5, "/static/images/photos/13.jpg", "本文だよー本文だよー13", now());
