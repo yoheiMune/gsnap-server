ce8ebb7b5d3595970f7c319705270a5b4e4393aa
Yohei Munesada <y.munesada@gmail.com>
2018-08-03 12:05:32 +0900

    ログインAPIを作成

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 import models
+from models import db, User, Post
 
 app = Flask(__name__)
 app.config.from_pyfile('.env')
@@ -9,6 +10,19 @@ models.init_db(app)
 @app.route("/")
 def index():
     return "Hello from flask"
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    login_id = request.form.get("login_id")
+    password = request.form.get("password")
+    if not login_id or not password:
+        return make_response(jsonify(message="Authorization failed."), 400)
+    user = db.session.query(User).filter_by(login_id=login_id, password=password).first()
+    if not user:
+        return make_response(jsonify(message="Authorization failed."), 400)
+    print(user)
+    return jsonify(user.as_dict())
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,11 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def init_db(app):
     db.init_app(app)
 
 
-class User(db.Model):
+class AbstractModel():
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name != "password"}
+
+
+class User(AbstractModel, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login_id = db.Column(db.String(255), nullable=False)
@@ -16,7 +22,8 @@ class User(db.Model):
     avatar_url = db.Column(db.String(255))
 
 
-class Post(db.Model):
+
+class Post(AbstractModel, db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, primary_key=True)
